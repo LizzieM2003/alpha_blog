542418a82dc670efa1e97085504cfda269f9febd
Lizzie M <liz_mendes2001@yahoo.co.uk>
2016-07-13 11:34:54 +0000

    Complete authentication system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  helper_method :current_user, :logged_in?
+  
+  def current_user
+    # return @current_user if it exists, otherwise find User based on session user_id
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    # !! turns any variable into a boolean
+    !!current_user
+  end
+  
+  def require_user
+    if !logged_in?
+      flash[:danger] = "You must be logged in to do that"
+      redirect_to root_path
+    end
+  end
 end
